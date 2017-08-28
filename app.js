5746c590bfd0d6cada93f33e7fc26c8b7651b943
John Bedette <jbedette@yahoo.com>
2017-08-28 15:49:34 -0400

    initial set up of events index

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const eventsRouter = require('./routes/events-routes');
+app.use('/events', eventsRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');

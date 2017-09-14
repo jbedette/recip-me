1b4e42042997a1495a5c3f6646be723f6471ea08
John Bedette <jbedette@yahoo.com>
2017-09-14 07:48:38 -0400

    heroku issues

--- app.js
+++ app.js
@@ -52,7 +52,6 @@ app.use('/user', userRoutes);
 const profileRoutes = require('./routes/profile-routes');
 app.use('/profile', profileRoutes);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`App is up and running. Listening on port ${PORT}`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
